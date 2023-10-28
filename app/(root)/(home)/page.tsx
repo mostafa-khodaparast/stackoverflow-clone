0592774cd1cb5a3b0a30277e800470e19cb2b85c
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2023-10-28 13:30:08 +0330

    add public folder & favicon

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from '@clerk/nextjs'
 export default function Home () {
   return (
     <div>
+      <h1>home page</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
-import './globals.css'
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'stack over flow',
-  description: 'this is a clone'
+  title: 'stackoverflow',
+  description: 'this is a clone',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
 }
 
 export default function RootLayout ({ children }: { children: React.ReactNode }) {
