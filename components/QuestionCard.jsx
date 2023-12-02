7b4570076dc621b5e684fdd0843e240cae052b49
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2023-12-02 14:13:38 +0330

    show author name and image in questioncart

--- components/QuestionCard.jsx
+++ components/QuestionCard.jsx
@@ -2,10 +2,11 @@ import Link from 'next/link'
 import React from 'react'
 import Tag from './Tag'
 import QuestionStats from './QuestionStats'
-// import Image from 'next/image'
+import Image from 'next/image'
 import { changeDateToString, formatNumber } from '@lib/utils'
 
 function QuestionCard ({ question }) {
+  console.log(question)
   return (
         <div className=' rounded-lg bg-light-700 p-4 font-semibold text-light-500 shadow-light-200 dark:bg-gradient-to-r dark:from-[#171c23] dark:to-[#13161c] dark:text-orange-500 dark:shadow-dark-100 md:p-6'>
             {/* title */}
@@ -25,18 +26,17 @@ function QuestionCard ({ question }) {
             <div className='mt-3 flex flex-col gap-1 sm:flex-row sm:items-center sm:justify-between'>
                 {/* author */}
                 <div className='flex items-center gap-1'>
-                    {/* <Link href={`/profile/${question.author._id}`} className='flex gap-1'> */}
-                    <Link href={`/profile/${question._id}`} className='flex gap-1'>
-                        {/* <Image
+                    <Link href={`/profile/${question.author._id}`} className='flex gap-1'>
+                    {/* <Link href={`/profile/${question._id}`} className='flex gap-1'> */}
+                        <Image
                             src={question.author.image}
                             alt={question.author.name}
                             width={20}
                             height={20}
-                            className=' rounded-full'
-                        /> */}
+                            className='rounded-full'
+                        />
                         <span className='text-sm text-stone-600 dark:text-orange-300'>
-                            {/* {question.author.name} */}
-                            {question.author}
+                            {question.author.name}
                         </span>
                     </Link>
                 </div>
