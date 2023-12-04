978085ad5ae0b056d605d114e94fb2f68894cfc9
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2023-12-04 14:40:47 +0330

    fix community user flex size

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -9,7 +9,7 @@ async function UserCard ({ user }) {
   const userInteractedTags = await getTopInteractedTags({ userId: user._id })
   
   return (
-    <Link href={`/profile/${user.clerkId}`} className='flex w-[32%] flex-col items-center space-y-2 rounded-md bg-light-700 px-6 py-2 font-semibold text-slate-700 dark:bg-dark-300 dark:text-orange-500'>
+    <Link href={`/profile/${user.clerkId}`} className='flex  w-1/2 md:w-1/3 flex-col items-center space-y-2 rounded-md bg-light-700 px-6 py-2 font-semibold text-slate-700 dark:bg-dark-300 dark:text-orange-500'>
         <Image
             src={user.picture}
             alt={user.name}
